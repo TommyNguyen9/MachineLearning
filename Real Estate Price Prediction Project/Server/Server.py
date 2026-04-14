e722834274452745eae62b097d2c0072d112211e
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-04-14 19:17:56 +0100

    Add files via upload

---
+++ Real Estate Price Prediction Project/Server/Server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import util
+
+
+app = Flask(__name__)
+
+@app.route('/get_location_names')
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+    return "Hello"
+
+
+if __name__ == "__main__":
+    print("Starting Flask Server For Home Price Prediction......")
+    app.run()
