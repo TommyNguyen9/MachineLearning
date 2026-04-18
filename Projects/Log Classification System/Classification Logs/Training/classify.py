7d103d670235088c3e7c3c1fae3d70cded9953e6
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-04-18 17:26:01 +0100

    Add files via upload

--- Projects/Log Classification System/Classification Logs/Training/classify.py
+++ Projects/Log Classification System/Classification Logs/Training/classify.py
@@ -2,14 +2,15 @@ from processor_regex import classify_with_regex
 from processor_bert import classify_with_bert
 from processor_llm import classify_with_llm
 
+import pandas as pd
+
 
 def classify(logs):
     labels = []
     for source, log_msg in logs:
         label = classify_log(source, log_msg)
-        labels.append((source, log_msg, label))
-        classify_log(source, logs)
-    return labels
+        labels.append((label))
+    print("\n".join(labels))
 
 
 
@@ -22,22 +23,33 @@ def classify_log(source, log_message):
             label = classify_with_bert(log_message)
     return label
 
+def classify_csv(input_file):
+    df = pd.read_csv(input_file)
+
+    df['target_label'] = classify(list(zip(df["source"], df["log_message"])))
+
+    output_file = "resources/output.csv"
+    df.to_csv(output_file, index = False)
+
 
 if __name__ == "__main__":
-    logs = [
-        ("ModernCRM", "IP 192.168.184.193 blocked due to potential attack!"),
-        ("BillingSystem", "User 9399 logged in."),
-        ("AnalyticsEngine", "File data_3940.csv uploaded successfully by user User998"),
-        ("ModernHR", "GET /v2/54fadb412c4e40cdbaed9335e4c35a9e/servers/detail HTTP/1.1 RCODE  200 len: 1583 time: 0.1878400"),
-        ("ModernHR", "Admin access escalation detected for User 9399!"),
-        ("LegacyCRM", "Case escalation for ticket ID 7324 failed because the assigned support agent is no longer active."),
-        ("LegacyCRM", "Invoice generation process aborted for order ID 8910 due to invalid tax calculation module."),
-        ("LegacyCRM", "The 'BulkEmailSender' feature is no longer supported. Use 'EmailCampaignManager' for improved functionality."),
-        ("LegacyCRM", " The 'ReportGenerator' module will be retired in version 4.0. Please migrate to the 'AdvancedAnalyticsSuite' by Dec 2025")
+    classify_csv("resources/test.csv")
 
-    ]
+    # logs = [
+    #     ("ModernCRM", "IP 192.168.184.193 blocked due to potential attack!"),
+    #     ("BillingSystem", "User User9399 logged in."),
+    #     ("AnalyticsEngine", "File data_3940.csv uploaded successfully by user User998"),
+    #     ("ModernHR", "GET /v2/54fadb412c4e40cdbaed9335e4c35a9e/servers/detail HTTP/1.1 RCODE  200 len: 1583 time: 0.1878400"),
+    #     ("ModernHR", "Admin access escalation detected for User 9399!"),
+    #     ("LegacyCRM", "Case escalation for ticket ID 7324 failed because the assigned support agent is no longer active."),
+    #     ("LegacyCRM", "Invoice generation process aborted for order ID 8910 due to invalid tax calculation module."),
+    #     ("LegacyCRM", "The 'BulkEmailSender' feature is no longer supported. Use 'EmailCampaignManager' for improved functionality."),
+    #     ("LegacyCRM", " The 'ReportGenerator' module will be retired in version 4.0. Please migrate to the 'AdvancedAnalyticsSuite' by Dec 2025")
 
-    classified_logs = classify(logs)
+    # ]
+
+    # classified_logs = classify(logs)
+    # print(classified_logs)
 
    
 
